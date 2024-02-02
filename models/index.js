83bbb156fecfd6a2bd9f42ce18f782f25f41b98d
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-02-02 01:05:19 -0500

    deleted files and added title and definition in html

--- models/index.js
+++ models/index.js
@@ -1,16 +1,3 @@
 const User = require('./User');
-const Post = require('./post')
 
-
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-});
-
-
-Post.belongsTo(User, {
-    foreignKey: 'user_id',
-    OnDelete: 'cascade'
-})
-
-
-module.exports = { User, Post };
+module.exports = { User };
