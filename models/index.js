3d825b9ee199d54b3c82dfa9b88cc2e1db16d351
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-02-08 02:44:24 -0500

    fixed my post seeds so that I could seed the data

--- models/index.js
+++ models/index.js
@@ -1,3 +1,26 @@
 const User = require('./User');
-// const Post = require('./Post')
-module.exports = { User };
+const Post = require('./Post')
+
+
+User.belongsTo(Post, {
+    foreignKey: "user_id"
+   
+ 
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: "post_id"
+  
+  });
+  
+  
+  
+
+
+
+
+
+
+module.exports = { User, Post };
+
+
