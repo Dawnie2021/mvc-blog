8e0fbcb1b98900d3804c2f478908213d675b7b01
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-02-22 18:02:34 -0500

    redirected to dashboard

--- routes/api/postRoutes.js
+++ routes/api/postRoutes.js
@@ -7,7 +7,7 @@ const { Post } = require('../../models');
 router.post('/', async (req, res) => {
     try {
         const postData = await Post.create(req.body);
-        res.status(200).json(postData);
+        res.redirect("/dashboard");
 
     } catch (err) {
         res.status(400).json(err);
